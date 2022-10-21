e20b0dbd59d8d46f3950a276b58167832cde16d2
eclaircy <116238694+eclaircy@users.noreply.github.com>
2022-10-21 10:56:17 +0800

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = "eclaircy's blogs"
+copyright = '2022, eclaircy'
+author = 'eclaircy'
 
 release = '0.1'
 version = '0.1.0'
